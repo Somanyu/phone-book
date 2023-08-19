4eb8de25de5a1c1c3e395ce9768f045a423deddc
Somanyu Samal <somanyu.03samal@gmail.com>
2023-08-19 08:59:40 +0530

    add: Query for adding contact to the list

--- src/config/queries.ts
+++ src/config/queries.ts
@@ -26,12 +26,39 @@ export const GetContactList = gql`
   }
 `;
 
+
+
 export const DeleteContactPhone = gql`
   mutation MyMutation($id: Int!) {
     delete_contact_by_pk(id: $id) {
       first_name
       last_name
       id
+    }
+  }
+`;
+
+export const AddContactWithPhones = gql`
+  mutation AddContactWithPhones(
+    $first_name: String!
+    $last_name: String!
+    $phones: [phone_insert_input!]!
+  ) {
+    insert_contact(
+      objects: {
+        first_name: $first_name
+        last_name: $last_name
+        phones: { data: $phones }
+      }
+    ) {
+      returning {
+        first_name
+        last_name
+        id
+        phones {
+          number
+        }
+      }
     }
   }
 `;
