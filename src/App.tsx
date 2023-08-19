c05b8880e57ab5a19c96c8b73cc1bb80dd9d14b0
Somanyu Samal <somanyu.03samal@gmail.com>
2023-08-19 12:05:25 +0530

    chore: Add route for edit and updated code for add

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ContactList from './components/ContactList';
 import AddContact from './components/AddContact';
+import EditContact from './components/EditContact';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ContactList />} />
-        <Route path="/add/contact" element={<AddContact />} />
+        <Route path="/contact/add" element={<AddContact />} />
+        <Route path="/contact/edit/:id" element={<EditContact />} />
       </Routes>
     </BrowserRouter>
   );
