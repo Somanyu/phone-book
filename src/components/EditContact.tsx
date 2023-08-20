ce2212a4be2ae94ee41ae71af3c113a390a54ae5
Somanyu Samal <somanyu.03samal@gmail.com>
2023-08-20 10:49:01 +0530

    chore: Edit a individual phone number of a contact

--- src/components/EditContact.tsx
+++ src/components/EditContact.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Input, useToast, FormControl, FormLabel, Center, Card, CardBody, Grid, Heading, GridItem, Flex, Button, Spacer, FormErrorMessage, HStack } from '@chakra-ui/react'
+import { Input, useToast, FormControl, FormLabel, Center, Card, CardBody, Grid, Heading, GridItem, Flex, Button, Spacer, FormErrorMessage, HStack, ButtonGroup, Popover, PopoverTrigger, IconButton, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader, PopoverBody } from '@chakra-ui/react'
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import PhoneInput from 'react-phone-input-2';
@@ -8,10 +8,10 @@ import { useMutation } from '@apollo/client';
 import 'react-phone-input-2/lib/style.css';
 
 // Config files
-import { GetContactDetail, EditContactById } from '../config/queries';
+import { GetContactDetail, EditContactById, EditPhoneNumber } from '../config/queries';
 import { Contact } from '../config/types';
-import { GoArrowLeft, GoCheck } from 'react-icons/go';
-import { Field, FieldArray, Form, Formik } from 'formik';
+import { GoArrowLeft, GoCheck, GoPencil } from 'react-icons/go';
+import { Field, Form, Formik } from 'formik';
 
 type Props = {}
 
@@ -22,6 +22,12 @@ const EditContact = (props: Props) => {
 
     const [contact, setContact] = useState<Contact | null>(null);
     const [updateContact] = useMutation(EditContactById);
+
+    // State to track the currently edited phone number
+    const [editingPhoneNumber, setEditingPhoneNumber] = useState('');
+
+    // Define the mutation function
+    const [editPhoneNumberMutation] = useMutation(EditPhoneNumber);
 
     const validateFirstName = (value: string) => {
         let error: string | undefined;
@@ -45,19 +51,6 @@ const EditContact = (props: Props) => {
             error = 'Only letters are allowed';
         } else if (value.length < 3 || value.length > 20) {
             error = 'Last name must be between 3 and 20 characters';
-        }
-        return error;
-    };
-
-    const validatePhone = (value: string) => {
-        let error: string | undefined;
-
-        if (!value) {
-            error = 'Phone number is required';
-        } else if (!/^[0-9]+$/.test(value)) {
-            error = 'Only letters are allowed';
-        } else if (value.length < 7 || value.length > 20) {
-            error = 'Number must be between 7 and 20 digits';
         }
         return error;
     };
@@ -91,7 +84,7 @@ const EditContact = (props: Props) => {
                                         phoneNumbers: contact?.phones.map((phone) => phone.number) || []
                                     }}
                                     onSubmit={async (values) => {
-                                        
+
                                         try {
                                             const updatedData = await updateContact({
                                                 variables: {
@@ -157,44 +150,108 @@ const EditContact = (props: Props) => {
                                             </Grid>
 
                                             <Grid templateColumns='repeat(5, 2fr)' gap={4}>
-                                                <FieldArray name="phoneNumbers">
-                                                    {({ push, remove }: any) => (
-                                                        <GridItem colSpan={3}>
-                                                            <FormLabel>Phone number</FormLabel>
-                                                            {contact?.phones.map((phone, index) => (
-                                                                <div key={index}>
-                                                                    <Field name={`phoneNumbers.${index}`} validate={validatePhone}>
-                                                                        {({ field, form }: any) => (
-                                                                            <FormControl isInvalid={form.errors.phoneNumbers?.[index] && form.touched.phoneNumbers?.[index]} isRequired>
-                                                                                <HStack>
-                                                                                    <PhoneInput
-                                                                                        value={phone.number}
-                                                                                        containerStyle={{ marginTop: '3.5px' }}
-                                                                                        inputStyle={{
-                                                                                            height: '2.5rem',
-                                                                                            fontSize: '1rem',
-                                                                                            borderRadius: '0.375rem',
-                                                                                            borderColor: 'inherit',
-                                                                                            background: 'inherit'
-                                                                                        }}
-                                                                                        onBlur={() => form.setFieldTouched(`phoneNumbers.${index}`, true)}
-                                                                                        onChange={(phoneNumber) =>
-                                                                                            form.setFieldValue(`phoneNumbers.${index}`, phoneNumber)
-                                                                                        }
-                                                                                    />
-                                                                                </HStack>
-                                                                                <FormErrorMessage>
-                                                                                    {form.errors.phoneNumbers?.[index]}
-                                                                                </FormErrorMessage>
-                                                                            </FormControl>
-                                                                        )}
-                                                                    </Field>
-                                                                </div>
-                                                            ))}
 
-                                                        </GridItem>
-                                                    )}
-                                                </FieldArray>
+                                                <GridItem colSpan={3}>
+                                                    <FormControl isReadOnly>
+                                                        <FormLabel>Phone number</FormLabel>
+                                                        {contact?.phones.map((phone, index) => (
+                                                            <div key={index}>
+                                                                <HStack>
+                                                                    <PhoneInput
+                                                                        value={phone.number}
+                                                                        containerStyle={{ marginTop: '3.5px' }}
+                                                                        inputStyle={{
+                                                                            height: '2.5rem',
+                                                                            fontSize: '1rem',
+                                                                            borderRadius: '0.375rem',
+                                                                            borderColor: 'inherit',
+                                                                            background: 'inherit'
+                                                                        }}
+                                                                    />
+
+                                                                    <Popover trigger='click' placement='right'>
+                                                                        <PopoverTrigger>
+                                                                            <IconButton size={'md'} aria-label='Edit contact' icon={<GoPencil />} />
+                                                                        </PopoverTrigger>
+                                                                        <PopoverContent>
+                                                                            <PopoverArrow />
+                                                                            <PopoverCloseButton />
+                                                                            <PopoverHeader>Update number</PopoverHeader>
+                                                                            <PopoverBody>
+                                                                                <>
+                                                                                    <FormControl isRequired>
+                                                                                        <FormLabel>Phone Number</FormLabel>
+                                                                                        <PhoneInput
+                                                                                            country="in"
+                                                                                            placeholder="Enter phone number"
+                                                                                            countryCodeEditable={false}
+                                                                                            containerStyle={{ marginTop: '3.5px' }}
+                                                                                            inputStyle={{
+                                                                                                height: '2.5rem',
+                                                                                                fontSize: '1rem',
+                                                                                                borderRadius: '0.375rem',
+                                                                                                borderColor: 'inherit',
+                                                                                                background: 'inherit',
+                                                                                            }}
+                                                                                            value={phone.number}
+                                                                                            onChange={(value) => setEditingPhoneNumber(value)}
+                                                                                        />
+                                                                                    </FormControl>
+                                                                                    <ButtonGroup mt={2} display='flex' justifyContent='flex-end'>
+                                                                                        <Button
+                                                                                            type='button'
+                                                                                            size={'sm'}
+                                                                                            colorScheme='whatsapp'
+                                                                                            onClick={async () => {
+                                                                                                try {
+                                                                                                    const updatedData = await editPhoneNumberMutation({
+                                                                                                        variables: {
+                                                                                                            pk_columns: {
+                                                                                                                contact_id: contact.id,
+                                                                                                                number: phone.number
+                                                                                                            },
+                                                                                                            new_phone_number: editingPhoneNumber 
+                                                                                                        }
+                                                                                                    });
+
+                                                                                                    if (updatedData && updatedData.data && updatedData.data.update_phone_by_pk) {
+                                                                                                        // Show toast for successful phone number update
+                                                                                                        toast({
+                                                                                                            title: 'Phone Number Updated',
+                                                                                                            description: 'Phone number has been successfully updated.',
+                                                                                                            status: 'success',
+                                                                                                            position: 'top',
+                                                                                                            duration: 3500,
+                                                                                                            isClosable: true,
+                                                                                                        });
+                                                                                                    }
+                                                                                                } catch (error) {
+                                                                                                    console.error('Error updating phone number: ', error);
+                                                                                                    // Show toast for unsuccessful phone number update
+                                                                                                    toast({
+                                                                                                        title: 'Error occurred',
+                                                                                                        description: `${error}`,
+                                                                                                        status: 'error',
+                                                                                                        position: 'top',
+                                                                                                        duration: 4000,
+                                                                                                        isClosable: true,
+                                                                                                    });
+                                                                                                }
+                                                                                            }}
+                                                                                        >
+                                                                                            Update
+                                                                                        </Button>
+                                                                                    </ButtonGroup>
+                                                                                </>
+                                                                            </PopoverBody>
+                                                                        </PopoverContent>
+                                                                    </Popover>
+
+                                                                </HStack>
+                                                            </div>
+                                                        ))}
+                                                    </FormControl>
+                                                </GridItem>
                                             </Grid>
                                             <Flex mt={10}>
                                                 <Link to='/'>
